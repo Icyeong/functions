d570c0d49495b1b7c668d40ad2fd219f3a7c2a36
chaeyeong Imm <cyim@chonf.co.kr>
2023-07-13 16:42:03 +0900

    불필요 코드 삭제

--- src/app/signup/components/Radio.tsx
+++ src/app/signup/components/Radio.tsx
@@ -1,13 +1,10 @@
 import Bs_Form from "react-bootstrap/Form";
 
-// type Props = [{ type: string; title: string }];
-
 type Props = {
   props: { type: string; title: string }[];
 };
 
 export default function Input({ props }: Props) {
-  console.log(props, props.length);
   return (
     <div className="mb-3">
       {props.length > 0 &&
