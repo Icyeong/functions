cb1c64362dce537716fa39590bab36fcb25a67b6
chaeyeong Imm <cyim@chonf.co.kr>
2023-07-17 16:54:31 +0900

    kakaopay 코드 추가

--- src/app/lib/payments.ts
+++ src/app/lib/payments.ts
@@ -6,6 +6,31 @@ export function naverpay() {
   return alert("Naverpay payment");
 }
 
-export function kakaopay() {
-  return alert("Kakaopay payment");
+export async function kakaopay() {
+  const fetchData = {
+    paymentId: 202305120001,
+    totalPrice: 10000,
+    goods: [
+      { goodsName: "포테토" },
+      { goodsName: "페퍼로니" }, //상품 여러개 보내도 첫번째 상품 정보만 사용함
+    ],
+    user: {
+      id: "abc123",
+      tel: "01012345678",
+      email: "aaa@aaa.com",
+    },
+    callbackUrl: `https://dapi-dev.dominos.co.kr/payment/kakaopay/result`,
+  };
+
+  const res = await fetch(
+    "https://dapi-dev.dominos.co.kr/payment/kakaopay/approval",
+    {
+      headers: {},
+      method: "POST",
+      body: JSON.stringify(fetchData),
+    }
+  );
+
+  console.log("kakaopay : ", console.log(res.json()));
+  return res.json();
 }
